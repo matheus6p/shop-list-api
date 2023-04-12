47be415ed7f1ccc585e26a35b120723a057bbf55
Matheus Paes <mhpaes97@gmail.com>
2023-04-12 19:04:44 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ db.once("open", () => console.log("Conexão com o banco feita com sucesso."));
 const app = express();
 
 app.use(cors());
-app.options("*", cors());
 app.use(express.json());
 app.use(ItemRoutes);
 
