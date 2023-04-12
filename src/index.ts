65d0eb91b3461f4156f7b5c3eec1ffd1898b2b1d
matheus6p <mhpaes97@gmail.com>
2023-04-12 09:02:19 -0300

    cors config

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ db.once("open", () => console.log("Conexão com o banco feita com sucesso."));
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use(ItemRoutes);
 
