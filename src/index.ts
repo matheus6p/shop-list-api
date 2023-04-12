a2afb3f0d096d11c6f4e27ac6a1180ea7a78c303
Matheus Paes <mhpaes97@gmail.com>
2023-04-12 17:17:08 -0300

    origin

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ db.once("open", () => console.log("Conexão com o banco feita com sucesso."));
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://shop-list-kappa.vercel.app"]
+}));
 app.use(express.json());
 
 app.use(ItemRoutes);
