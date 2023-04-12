151e9247304a8487ebeaa4b7001e0fa278baaa8d
Matheus Paes <mhpaes97@gmail.com>
2023-04-12 17:45:41 -0300

    cors

--- src/controller/ItemController.ts
+++ src/controller/ItemController.ts
@@ -4,6 +4,7 @@ import { ItemService } from "../services/ItemService";
 const itemService = new ItemService();
 
 export async function getItems(req: Request, res: Response) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://shop-list-kappa.vercel.app/')
   try {
     const items = await itemService.getAllItems();
     return res.status(200).json(items);
